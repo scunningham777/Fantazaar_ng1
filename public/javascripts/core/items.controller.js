2633f57b6dd411af270c8fe7564a84e5641102d5
scunningham777 <s.cunningham777@gmail.com>
2016-02-26 17:58:47 -0600

    Started work on items controller

--- public/javascripts/core/items.controller.js
+++ public/javascripts/core/items.controller.js
@@ -6,9 +6,33 @@
         .controller('ItemsController', ItemsController);
 
     /*@ngInject*/
-    function ItemsController($state) {
+    function ItemsController() {
         var vm = this;
 
-        vm.items = [];
+        vm.items = {};      //use item.id as the key, and item as the value
+        vm.modifyItemOwnedCount = modifyItemOwnedCount;
+        vm.modifyItemSoldCount = modifyItemSoldCount;
+        vm.incrementItemOwnedCount = incrementItemOwnedCount;
+        vm.transferCountFromOwnedToSold = transferCountFromOwnedToSold;
+
+
+        function modifyItemOwnedCount(item, newItemOwnedCount) {
+            if (!vm.items.hasOwnProperty(item._id + "")) {
+                return;
+            }
+        }
+
+        function modifyItemSoldCount(item, newItemSoldCount) {
+
+        }
+
+        function incrementItemOwnedCount(item) {
+            editItemOwnedCount(item, 1);
+        }
+
+        function transferCountFromOwnedToSold(item, countTransferred) {
+            editItemOwnedCount(item, countTransferred * -1);
+            editItemSoldCount(item, countTransferred);
+        }
     }
 })();
